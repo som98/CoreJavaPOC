50c36c2909d69c20f3c109518d0b4b662603b825
Somshubhra Roy <roysomshubhra98@gmail.com>
2023-02-28 23:19:06 +0530

    map() Vs flatMap() mapper functions in Java8

---
+++ CoreJava/src/com/practice/som/java8/mapVsflatMap/CustomerDAO.java
@@ -0,0 +1,31 @@
+package com.practice.som.java8.mapVsflatMap;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CustomerDAO {
+	
+	public static List<Customer> getAllDetails(){
+		
+		List<Customer> list = new ArrayList<>();
+		list.add(new Customer(101, "john", "john@gmail.com", Arrays.asList("397937955", "21654725")));
+		list.add(new Customer(102, "smith", "smith@gmail.com", Arrays.asList("89563865", "2487238947")));
+		list.add(new Customer(103, "peter", "peter@gmail.com", Arrays.asList("38946328654", "3286487236")));
+		list.add(new Customer(104, "kely", "kely@gmail.com", Arrays.asList("389246829364", "948609467")));
+		
+		return list;
+		
+		
+		//Alternative way to add Array of objects into a list and return
+		 /*return Stream.of(
+	                new Customer(101, "john", "john@gmail.com", Arrays.asList("397937955", "21654725")),
+	                new Customer(102, "smith", "smith@gmail.com", Arrays.asList("89563865", "2487238947")),
+	                new Customer(103, "peter", "peter@gmail.com", Arrays.asList("38946328654", "3286487236")),
+	                new Customer(104, "kely", "kely@gmail.com", Arrays.asList("389246829364", "948609467"))
+	        ).collect(Collectors.toList());
+	        */	
+		
+	}
+
+}
