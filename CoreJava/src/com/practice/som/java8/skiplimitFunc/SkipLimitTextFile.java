90c1e95a78fdd460ce814b792be9e976fd962967
Somshubhra Roy <roysomshubhra98@gmail.com>
2023-03-23 22:55:17 +0530

    skip and limit methods of Stream API Java8

---
+++ CoreJava/src/com/practice/som/java8/skiplimitFunc/SkipLimitTextFile.java
@@ -0,0 +1,22 @@
+package com.practice.som.java8.skiplimitFunc;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class SkipLimitTextFile {
+
+	public static void main(String[] args) throws IOException {
+		
+		//Reading data from file into a List using nio.File
+		List<String> listData= Files.readAllLines(Paths.get("data.txt"));
+		
+		//Skipping the header from the file and the last line
+		listData.stream()
+		.skip(1)
+		.limit(listData.size()-2)
+		.forEach(System.out::println);
+	}
+
+}
